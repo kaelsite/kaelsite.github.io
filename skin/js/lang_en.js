bf1403d71a4c52867549eab477e439b312895e03
breathelife <1447215256@qq.com>
2019-06-02 17:40:33 +0800

    up
    up

---
+++ skin/js/lang_en.js
@@ -0,0 +1,69 @@
+window.LANG_EN = {
+    lang: '中文',
+    nav: {
+        home: 'HOME',
+        brand: 'BRAND STORY',
+        shop: 'SHOP',
+        about: 'ABOUT CBD',
+        contact: 'CONTACT US',
+        product_1: 'PRODUCT A',
+        product_2: 'PRODUCT B',
+        product_3: 'PRODUCT C'
+    },
+    brand_story: {
+        benefit: 'OUR STORY',
+        content_1: 'Hemplus, a skincare pioneer from the United States, is a brand of Hempup naturals. It was born in Colorado, United States targeting the US high-end CBD-infused skin care brand. Hempup naturals is committed to the development of top natural organic skin care products, and explores nature with the natural philosophy of homeopathic therapy.  We are dedicated to provide the most natural and pure skin care products. Our philosophy is that Nature is the most dominant ruler of all. Reveal the magic of the rare species essence, awaken the skin and vitality. Combine natural biological essence with modern technology to create skin-friendly, safe, Long-lasting, self-healing natural beauty products.',
+        content_2: 'All raw materials and finished products have undergone strict quality inspection and testing. The products have passed the EU CE EU ROHS certification and the US FDA (U.S. Food and Drug Administration) certification.'
+    },
+    shop: {
+        title: 'OUR PRODUCTS',
+        product_1: {
+            name: '大麻赋活眼部精华乳',
+            price: '$ 1580',
+            feature: '150ml',
+            benefit: 'Absolute Rose 80',
+            rating: 5,
+            pic: 'hem_1.jpg',
+            product_info: '产品功效说明非成分表',
+            product_info_detail: '产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表',
+            use_info: '使用说明',
+            use_info_detail: '使用说明使用说明使用说明使用说明使用说明'
+        },
+        product_2: {
+            name: '大麻赋活眼部精华乳',
+            price: '$ 1580',
+            feature: '150ml',
+            benefit: 'Absolute Rose 80',
+            rating: 4,
+            pic: 'hem_1.jpg',
+            product_info: '产品功效说明非成分表',
+            product_info_detail: '产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表',
+            use_info: '使用说明',
+            use_info_detail: '使用说明使用说明使用说明使用说明使用说明'
+        },
+        product_3: {
+            name: '大麻赋活眼部精华乳',
+            price: '$ 1580',
+            feature: '150ml',
+            benefit: 'Absolute Rose 80',
+            rating: 5,
+            pic: 'hem_1.jpg',
+            product_info: '产品功效说明非成分表',
+            product_info_detail: '产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表',
+            use_info: '使用说明',
+            use_info_detail: '使用说明使用说明使用说明使用说明使用说明'
+        }
+    },
+    about_cbd: {
+        benefit: 'Award Winning MONDO formula now in CBD!',
+        info_title: 'WHAT IS CBD?',
+        info: 'Our full-spectrum CBD powder has been crafted from all organically grown hemp and expertly blended with the same non-GMO organic ingredients that you know and love in our original MONDO formula plus theanine. MONDO CBD contains 150 mg of CBD, contains no artificial colors or ingredients, is sugar and gluten free, and 100% vegan friendly!',
+        feature_1: 'MONDO uses whole plant hemp oil in all products from 100% organic industrial hemp. This oil contains naturally occurring cannabinoids including CBD expertly extracted to retain the integrity of the plants original phytocannabinoids and terpenes. Our CBD is a natural constituent of industrial hemp.',
+        feature_2: 'People use theanine for treating anxiety and high blood pressure, for preventing Alzheimer’s disease, and for making cancer drugs more effective.'
+    },
+    contact_us: {
+        title: 'CONTACT US',
+        email: 'Email: xxxx@qq.com',
+        address: 'Address: 华盛顿XXXXXX'
+    }
+}
---
+++ skin/js/lang_zh.js
@@ -0,0 +1,69 @@
+window.LANG_ZH = {
+    lang: 'EN',
+    nav: {
+        home: 'HOME',
+        brand: '品牌故事',
+        shop: 'SHOP',
+        about: '关于 CBD',
+        contact: '联系我们',
+        product_1: 'PRODUCT A',
+        product_2: 'PRODUCT B',
+        product_3: 'PRODUCT C'
+    },
+    brand_story: {
+        benefit: '我们的故事',
+        content_1: 'Hemplus，来自美国的CBD护肤先驱，属Hempup naturals 旗下品牌，诞生于美国科罗拉多州，美国上市高端美护CBD品牌，Hempup naturals致力于顶级天然有机护肤品研发，秉承顺势疗法的自然哲学探索大自然的奥义提供最自然精纯的护肤产品.坚持Nature is the most dominant ruler of all.解密珍稀物种精华中富蕴其中的奇异能量，唤醒肌肤生机.汲取天然生物精萃与现代科技融合打造亲肤、安全、长效、自愈的天然美肌产品.',
+        content_2: '所有原料以及制成品都经过严格品质检控筛选，产品通过欧盟CE欧盟ROHS有效认证以及美国FDA（U.S. Food and Drug Administration）认证.'
+    },
+    shop: {
+        title: '我们的产品',
+        product_1: {
+            name: '大麻赋活眼部精华乳',
+            price: '￥1580',
+            feature: '150ml',
+            benefit: 'Absolute Rose 80',
+            rating: 5,
+            pic: 'hem_1.jpg',
+            product_info: '产品功效说明非成分表',
+            product_info_detail: '产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表',
+            use_info: '使用说明',
+            use_info_detail: '使用说明使用说明使用说明使用说明使用说明'
+        },
+        product_2: {
+            name: '大麻赋活眼部精华乳',
+            price: '￥1580',
+            feature: '150ml',
+            benefit: 'Absolute Rose 80',
+            rating: 5,
+            pic: 'hem_1.jpg',
+            product_info: '产品功效说明非成分表',
+            product_info_detail: '产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表',
+            use_info: '使用说明',
+            use_info_detail: '使用说明使用说明使用说明使用说明使用说明'
+        },
+        product_3: {
+            name: '大麻赋活眼部精华乳',
+            price: '￥1580',
+            feature: '150ml',
+            benefit: 'Absolute Rose 80',
+            rating: 5,
+            pic: 'hem_1.jpg',
+            product_info: '产品功效说明非成分表',
+            product_info_detail: '产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表产品功效说明非成分表',
+            use_info: '使用说明',
+            use_info_detail: '使用说明使用说明使用说明使用说明使用说明'
+        }
+    },
+    about_cbd: {
+        benefit: '现在在CBD获奖的MONDO公式',
+        info_title: 'WHAT IS CBD?',
+        info: '我们的全光谱CBD粉末由所有有机种植的大麻精制而成，并且与我们原始的MONDO配方和茶氨酸中您熟知和喜爱的相同非转基因有机成分巧妙地混合。 MONDO CBD含有150毫克中央商务区，不含人工色素或成分，不含糖和麸质，100％素食主义者友好！',
+        feature_1: 'MONDO在100％有机工业大麻的所有产品中使用全植物大麻油。 这种油含有天然存在的大麻素，包括专业提取的CBD，以保持植物原始植物大麻素和萜烯的完整性。 我们的CBD是工业大麻的天然成分。',
+        feature_2: '人们使用茶氨酸治疗焦虑和高血压，预防阿尔茨海默病，以及使癌症药物更有效。'
+    },
+    contact_us: {
+        title: '联系我们',
+        email: '邮箱：xxxx@qq.com',
+        address: '地址：华盛顿XXXXXX'
+    }
+}
